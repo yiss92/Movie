56a66e3f5eeb4e37fb09fe7f49704bcdf5e7aa43
USER <USER@hb2004>
2015-09-22 14:40:37 +0900

    insert member

--- src/service/MemberService.java
+++ src/service/MemberService.java
@@ -1,8 +1,12 @@
 package service;
 
 import java.sql.SQLException;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
+
+import repository.UserDao;
+import vo.User;
 
 public class MemberService {
 	// singleton
@@ -16,10 +20,39 @@ public class MemberService {
 	}
 
 	public int registeMember(HttpServletRequest request) throws ClassNotFoundException, SQLException {
+		 //USER_CD, USER_ID, PW, NICKNAME, EMAIL, YMD
+//		pstmt.setString(1, "user");
+//		pstmt.setString(2, user.getUserId());
+//		pstmt.setString(3, user.getPw());
+//		pstmt.setString(4, user.getNickname());
+//		pstmt.setString(5, user.getEmail());
+//		pstmt.setTimestamp(6,new Timestamp(user.getYmd().getTime()));
+		String member_id = request.getParameter("member_id");
+		String member_pw = request.getParameter("member_pw");
+		String member_nic = request.getParameter("nickname");
+		String member_email = request.getParameter("email");
 		
-		int member_id = Integer.parseInt(request.getParameter("member_id"));
+		User user = new User();
+		user.setUserCd(member_id);
+		user.setPw(member_pw);
+		user.setNickname(member_nic);
+		user.setEmail(member_email);
+		user.setYmd(new Date());
+		
+		UserDao userDao = UserDao.getInstance();
+		userDao.startCon();
+		
+		int result = userDao.insertUser(user);
+		userDao.closeCon();
+//		MemberDao dao = MemberDao.getInstance();
+//		dao.startConnection();
+//		
+//		int result = dao.insertMember(member);
+//		dao.closeConnection();
 
-		return 0;
+		return result;	
 	}
+	
+	
 
 }
