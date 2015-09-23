305b5c4131b1e7ef3b644692239fe49347047573
caria <caria@DESKTOP-MBDEBBL>
2015-09-23 14:52:09 +0900

    update FreeDao

--- src/repository/FreeDao.java
+++ src/repository/FreeDao.java
@@ -50,7 +50,7 @@ public class FreeDao{
 		PreparedStatement pstmt = null;
 		int result=0;
 		try {
-			String sql="insert into FREE_ARTICLE_TB( USER_ID, ARTICLE_TITLE, CONTENT,, YMD)"
+			String sql="insert into FREE_ARTICLE_TB( USER_ID, ARTICLE_TITLE, CONTENT, YMD)"
 					+"values(?,?,?,?)";
 			pstmt=con.prepareStatement(sql);
 			pstmt.setString(1, freeArticle.getUserId());
